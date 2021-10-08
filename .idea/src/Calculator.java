3c6b0407ea48410538f2dc99ebfbfd1e96c19b39
Musti992201 <Mustafa.Uyar@outlook.at>
2021-10-08 20:02:53 +0200

    Variablen deklarieren

--- .idea/src/Calculator.java
+++ .idea/src/Calculator.java
@@ -4,8 +4,12 @@ public static void main(String[] args){
     int a= 1;
     System.out.println(a);
     double b=2;
-
-
-
+    System.out.println(b);
+    boolean c=true;
+    System.out.println(c);
+    char d= 'd';
+    System.out.println(d);
+    String s="Ich bin ein String";
+    System.out.println(d);
 }
 }
